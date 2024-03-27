7dbc7949868ea335eaef28583f5beb02226cd94f
Practic73 <sierrachart@yandex.ru>
2024-03-27 16:40:36 +0300

    add

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser import views
+
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
