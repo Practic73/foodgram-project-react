659424b7d62d20bf3983c067d503523866a0575a
Practic73 <sierrachart@yandex.ru>
2024-03-28 12:00:25 +0300

    deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', default='key')
 
 
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS =  os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
