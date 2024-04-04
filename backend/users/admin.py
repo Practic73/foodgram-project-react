a6acc1980898d7c404b486fbed166885dadbf756
Practic73 <sierrachart@yandex.ru>
2024-04-04 14:28:27 +0300

    Модель пользователя. Админка пользователя

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Subscription, User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email')
+    list_filter = ('email', 'first_name', 'last_name')
+    search_fields = ('email', 'first_name', 'last_name')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('author', 'user')
+    list_filter = ('author', 'user')
+    search_fields = ('author',)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=True)
+    username = models.CharField(' Логин', max_length=150, unique=True)
+    first_name = models.CharField('Имя', max_length=150)
+    last_name = models.CharField('Фамилия', max_length=150)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+
+class Subscription(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='author_subscription',
+        verbose_name='На кого подписались',
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user_subscription',
+        verbose_name='Кто подписался',
+    )
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'Подписки'
